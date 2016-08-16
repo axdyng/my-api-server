660aa10297d37307f5efe2ab8768f9cd857ee1cf
Dylan Ng <ngxinde@gmail.com>
2016-08-17 01:50:41 +0800

    update json

--- app/controllers/dylan.controller.js
+++ app/controllers/dylan.controller.js
@@ -1,9 +1,9 @@
 module.exports = {
   index: function(req, res) {
     res.jsonp({
-      profileURL: 'http://v030o.popscreen.com/eHNqbWd6MTI=_o_pokemon-profiles-dragonite-149.jpg',
+      profileURL: 'http://pm1.narvii.com/5847/30a4b49a069109f96ee55b61b2788d496095977a_hq.jpg',
       name: 'Dylan',
-      profession: 'Pokemon Trainer',
+      profession: 'Trainer',
       quote: "A caterpie may change into a butterfree, but the heart beating inside remains the same."
     });
   }
