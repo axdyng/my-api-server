24eb38e55c133521b44fd505742f4d8d485bd559
Dylan Ng <ngxinde@gmail.com>
2016-08-17 10:36:42 +0800

    making error

--- app/controllers/dylan.controller.js
+++ app/controllers/dylan.controller.js
@@ -4,7 +4,7 @@ module.exports = {
       profileURL: 'http://pm1.narvii.com/5847/30a4b49a069109f96ee55b61b2788d496095977a_hq.jpg',
       name: 'Dylan',
       profession: 'Trainer',
-      quote: "A caterpie may change into a butterfree, but the heart beating inside remains the same."
+      quote: "A caterpie may change into a butterfree, but the heart beating inside remains the sameaaaaa."
     });
   }
 };
